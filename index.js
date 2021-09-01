38d81c34c5b7af5f83598a39564a5d61dd1f92e7
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-09-01 09:29:44 +0700

    feature(graceful_exit): added graceful exit handling

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = require('./app')
 
 const server = http.createServer(app)
 
+process.on('SIGINT', () => {
+    server.close()
+    console.log('server closed')
+})
+
 server.listen(8080, () => {
-	console.log("server listens on port 8080")
+    console.log('server listens on port 8080')
 })
